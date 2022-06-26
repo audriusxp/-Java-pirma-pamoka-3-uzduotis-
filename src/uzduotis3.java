9dd07bdebad8e6c614faf81f5163a0d3ed53d486
audriusxp <palm@inbox.lt>
2022-06-26 10:35:44 +0300

    pradeta uzduotis

---
+++ src/uzduotis3.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class uzduotis3 {
+    public static void main(String[] args) {
+        double a;
+        Scanner skaitytuvas = new Scanner(System.in);
+        System.out.println("iveskite krastine A : ");
+        a = skaitytuvas.nextInt();
+        System.out.println("krastinesIlgisA = " + a);
+
+        double b;
+        Scanner skaitytuvas1 = new Scanner(System.in);
+        System.out.println("iveskite krastine B : ");
+        b = skaitytuvas1.nextInt();
+        System.out.println("krastinesIlgisB = " + b);
+
+        double c;
+        Scanner skaitytuvas2 = new Scanner(System.in);
+        System.out.println("iveskite krastine C : ");
+        c = skaitytuvas2.nextInt();
+        System.out.println("krastinesIlgisC = " + c);
+
+
+
+    }
+
+    public static double GalimasTrikampis(double a, double b, double c) {
+        if (a + b > c) {
+            if (b + c > a) {
+                if (c + a > b) {
+                    System.out.println("true = " + true);
+                }
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
