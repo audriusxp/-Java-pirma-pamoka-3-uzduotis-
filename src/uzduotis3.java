ad0b1296084d7f4170cdc880058663faba941af3
audriusxp <palm@inbox.lt>
2022-06-27 12:14:39 +0300

    pakeista skaneryje is in i duoble

--- src/uzduotis3.java
+++ src/uzduotis3.java
@@ -5,36 +5,43 @@ public class uzduotis3 {
         double a;
         Scanner skaitytuvas = new Scanner(System.in);
         System.out.println("iveskite krastine A : ");
-        a = skaitytuvas.nextInt();
+        a = skaitytuvas.nextDouble();
         System.out.println("krastinesIlgisA = " + a);
 
         double b;
         Scanner skaitytuvas1 = new Scanner(System.in);
         System.out.println("iveskite krastine B : ");
-        b = skaitytuvas1.nextInt();
+        b = skaitytuvas1.nextDouble();
         System.out.println("krastinesIlgisB = " + b);
 
         double c;
         Scanner skaitytuvas2 = new Scanner(System.in);
         System.out.println("iveskite krastine C : ");
-        c = skaitytuvas2.nextInt();
+        c = skaitytuvas2.nextDouble();
         System.out.println("krastinesIlgisC = " + c);
+        System.out.println("GalimasTrikampis(a, b, c) = " + galimasTrikampis(a, b, c));
+
 
 
 
     }
 
-    public static double GalimasTrikampis(double a, double b, double c) {
+
+
+
+    public static boolean galimasTrikampis(double a, double b, double c) {
         if (a + b > c) {
             if (b + c > a) {
                 if (c + a > b) {
                     System.out.println("true = " + true);
                 }
 
-
-
+            }
+        }
+        return false;
     }
 }
+
 
 
 
